dbaf9368f0fd47e3c8bb1434d99bf287b265126e
EUROHELP-BILBAO\jaochoa <jaochoa@euro-help.es>
2017-08-08 12:47:41 +0200

    Termindo separar string en array

--- index.php
+++ index.php
@@ -2,38 +2,22 @@
 
 	$cadena = "[:en]https://play.google.com/store/apps/ EN 2 [:de]https://play.google.com/store/apps/ DE[:eu]https://play.google.com/store/apps/ EU[:]";
 	
-	#Cortamos las frases entre :
-	#
-	$array = array('[:en]', '[:es]', '[:de]', '[:eu]', '[:fr]', '[:it]');
-
-	$numIdiomas = sizeof($array);
-	for($i = 0; $i < $numIdiomas; $i++){
-		preg_split("/\[:..\]/", $cadena);
-		#$array = array(explode('/\[:\]/', $cadena));
-		switch ($i) {
-			case '[:es]':
-				# code...
-				break;
-			case '[:de]':
-				# code...
-				break;
-			case '[:eu]':
-				# code...
-				break;
-			case '[:fr]':
-				# code...
-				break;	
-			case '[:it]':
-				# code...
-				break;
-			default:
-				# code...
-				break;
-		}
-		
-
+	
+	//Cortamos la cadena para quitar el final
+	$cadena = substr($cadena, 0, -3);
+	//idiomas
+	preg_match_all("/\[:..\]/", $cadena, $idi);
+	//textos
+	$content = preg_split("/\[:..\]/", $cadena, -1, PREG_SPLIT_NO_EMPTY);
+	
+	for($i = 0; $i < sizeof($content); $i++){
+		$final[$i][0] = $idi[0][$i];
+		$final[$i][1] = $content[$i];
 		
 	}
-	var_dump($array);
-	
+	var_dump($final);
+
+	#OTRA FORMA
+	$claves = preg_split("/\[|\]/", $cadena);
+	var_dump($claves);
 ?>
